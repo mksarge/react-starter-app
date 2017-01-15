724be34ab037324e18a8d0300d56681a89ee7188
Michael Sargent <michaelksarge@gmail.com>
2017-01-14 19:50:05 -0800

    Update Header and Navbar

--- app/components/NavBar/Navbar.js
+++ app/components/NavBar/Navbar.js
@@ -3,28 +3,23 @@ import { browserHistory, Link } from 'react-router';
 import css from './Navbar.css';
 
 const HighlightedLink = (props) => {
-  const path = browserHistory.getCurrentLocation().pathname;
-  const path2 = path.slice(1);
-  const index = path2.indexOf('/');
-  let path3;
-  if (index !== -1) {
-    path3 = path2.slice(0, index);
-  } else {
-    path3 = path2;
-  }
-  const match = props.to === `/${path3}`;
-  return (
-    <Link
-      className={css.navblock}
-      style={{
-        background: match ? '#a0c0ff' : 'none',
-        color: match ? 'white' : '#24b',
-      }}
-      to={props.to}
-    >
-      {props.title}
-    </Link>
-  );
+  // get url pathname without leading '/'
+  let path = browserHistory.getCurrentLocation().pathname.slice(1);
+
+  // check if there is another '/' in the url, and slice it off
+  const index = path.indexOf('/');
+  path = (index !== -1) ? path.slice(0, index) : path;
+
+  // check if sliced url matches one of the three nav links
+  const match = props.to === `/${path}`;
+
+  // highlight Link style if the url matches
+  const style = {
+    background: match ? '#236' : 'none',
+    color: match ? 'white' : '#24b',
+  };
+
+  return <Link className={css.navblock} to={props.to} style={style}>{props.title}</Link>;
 };
 
 HighlightedLink.propTypes = {
