c1c49f35f8574ae2f29c492ab7555af86286aa16
Michael Sargent <michaelksarge@gmail.com>
2017-01-19 23:05:12 -0800

    Update config files

--- app/env.js
+++ app/env.js
@@ -3,14 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
   env,
   header: 'React Starter App',
-  project: 'react-starter-app',
   github: 'https://github.com/mksarge/react-starter-app',
 };
-
-// alternative settings for test environment
-// if (env === 'test') { }
-
-// alternative settings for production environment
-// if (env === 'production') { }
 
 export default config;
--- app/posts.js
+++ app/posts.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-dynamic-require, global-require */
+
 // declare your active blog posts here
 const titles = [
   'sample-post-1.md',
   'sample-post-2.md',
 ];
 
-const posts = titles.map((title) => require(`../posts/${title}`)); // eslint-disable-line import/no-dynamic-require, global-require
+const posts = titles.map((title) => require(`../posts/${title}`));
 
 export default posts;
