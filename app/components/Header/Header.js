90a242f8ee2b197362c5ed5945e68ba6a7fbd217
Michael Sargent <michaelksarge@gmail.com>
2017-01-20 00:24:30 -0800

    Use GitHub 'corner' instead of 'ribbon'

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import Corner from './Corner';
 import css from './Header.css';
 import config from '../../env';
 import { getRootPath } from '../../utils';
@@ -8,11 +9,7 @@ const Header = (props) => {
   const path = getRootPath();
   return (
     <div className={path === 'home' ? [css.header, css.header2].join(' ') : css.header}>
-      <div className={css.ribbon}>
-        <a href={config.github}>
-          <img src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" />
-        </a>
-      </div>
+      {path === 'home' && (<Corner link={config.github} />)}
       <div className={css.title}>
         <Link to="/">
           {path === 'home' && (<img className={css.logo} src="react-logo.png" alt="" />)}
