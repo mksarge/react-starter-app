5388f825c22dffad2bc310c443fe8ffbb82697ea
Michael Sargent <michaelksarge@gmail.com>
2017-01-19 22:44:20 -0800

    Remove Footer's dependency on markdown

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import css from './Footer.css';
-import { html } from './Footer.md';
 
 const Footer = () => (
   <footer className={css.footer}>
-    <div dangerouslySetInnerHTML={{ __html: html }} />
+    <p>
+      React Starter App is on&nbsp;
+      <a href="https://github.com/mksarge/react-starter-app">GitHub</a>.
+      <br />
+      <a href="https://react-starter-app.firebaseapp.com/docs">Docs</a>
+      &nbsp;&middot;&nbsp;
+      <a href="https://github.com/mksarge/react-starter-app/issues">Report an issue</a>
+      &nbsp;&middot;&nbsp;
+      <a href="https://github.com/mksarge/react-starter-app/fork">Contribute</a>
+    </p>
   </footer>
 );
 
