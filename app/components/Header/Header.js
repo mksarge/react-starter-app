31fb3cdaa7c054b0597a9e34865ea793378d026a
Michael Sargent <michaelksarge@gmail.com>
2017-04-20 23:16:06 -0700

    Use svg logo

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router';
 import Corner from './Corner';
 import css from './Header.css';
 import config from '../../../config/config.json';
+import reactLogo from './react.svg';
 
 const Header = () => (
-  <div className={[css.header, css.header2].join(' ')}>
+  <div className={css.header}>
     <Corner link={config.github} />
     <div className={css.title}>
       <Link to="/">
-        <img className={css.logo} src={config.logo} alt="" />
+        <img className={css.logo} src={reactLogo} alt="" />
         {config.name}
       </Link>
     </div>
